d8b98702b76cbef5c52aef224f007de3af74171f
glinzachariah <glinzac@gmail.com>
2018-11-06 23:19:15 +0530

    completed prgm5

--- day5/prgm.c
+++ day5/prgm.c
@@ -6,6 +6,8 @@
 int num_prod,nonter=0;
 char input[20][20];
 char first[20][20];
+char follow[20][20];
+char epi =238;
 
 
 char calFirst(char letter,int i){
@@ -14,7 +16,7 @@ char calFirst(char letter,int i){
 			if(islower(input[i][2])){
 				return input[i][2];
 			}else{
-				calFirst(input[i][2],i+1);
+				return calFirst(input[i][2],i+1);
 			}
 		}
 	}
@@ -26,7 +28,7 @@ void main(){
 	printf("Enter the number of productions: \t");
 	scanf("%d",&num_prod);
 	for(i=0;i<num_prod;i++){
-		printf("Enter the production %d : \n",i+1);
+		printf("Enter the production %d : \t",i+1);
 		scanf("%s",input[i]);
 	}
 	
@@ -35,6 +37,8 @@ void main(){
 		if(nonter<1){
 			first[0][0] =input[0][0];
 			first[0][1]='\0';
+			follow[0][0] =input[0][0];
+			follow[0][1]='\0';
 			nonter++;
 		}else{
 			int flag=0;
@@ -47,6 +51,8 @@ void main(){
 			if(flag ==0){
 					first[nonter][0]=input[i][0];
 					first[nonter][1]='\0';
+					follow[nonter][0]=input[i][0];
+					follow[nonter][1]='\0';
 					nonter++;
 			}
 		}
@@ -69,8 +75,59 @@ void main(){
 			}
 		}
 	}
-	printf("count is %d ",nonter);
+
+	//calculating follow
 	for(i=0;i<nonter;i++){
-		printf("\n%d = %s\n",i+1,first[i]);	
+		for(j=0;j<num_prod;j++){
+			int k =strlen(input[j]);
+			int l;
+			for(l=2;l<k;l++){
+				if(isupper(input[j][l])){
+					if(input[j][l]==follow[i][0]){
+					if(l+1<k){
+						if(islower(input[j][l+1])){
+							int len = strlen(follow[i]);
+							follow[i][len] = input[j][l+1];
+							follow[i][len+1] ='\0';
+						}else{
+							char text =calFirst(input[j][l+1],0);
+							int len = strlen(follow[i]);
+							follow[i][len] = text;
+							follow[i][len+1] ='\0';
+						}
+						
+					}else{
+							int len = strlen(follow[i]);
+							follow[i][len] = epi;
+							follow[i][len+1] ='\0';						
+					}
+					}
+				}
+			}			
+		}
+	}
+
+	//displaying the first and follow
+	for(i=0;i<nonter;i++){
+		if(strlen(first[i])==1){
+			first[i][1] =epi;
+			first[i][2] ='\0';
+
+		}
+		//printf("\n%d FIRST=> %s\tFOLLOW=> %s  \n",i+1,first[i],follow[i]);
+		printf("------------\t");		
+		printf("\nFIRST of %c : ",first[i][0]);
+		int len = strlen(first[i]);
+		int j;		
+		for(j=1;j<len;j++){
+			printf("%c,", first[i][j]);
+		}	
+		printf("\n");
+		printf("FOLLOW of %c : ",follow[i][0]);
+		int length = strlen(follow[i]);		
+		for(j=1;j<length;j++){
+			printf("%c," ,follow[i][j]);
+		}	
+		printf("\n");
 	}
 }
